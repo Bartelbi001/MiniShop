3739256fe592a21ab500aa08732bb676027da903
Ilya Shelkunov <shelkunov.20014@gmail.com>
2025-10-24 15:46:42 +0300

    git commit -m "chore(api): minor cleanup in Program.cs imports and middleware order"

--- src/MiniShop.Api/Program.cs
+++ src/MiniShop.Api/Program.cs
@@ -1,3 +1,8 @@
+using MiniShop.Api.Extensions;
+using MiniShop.Application;
+using MiniShop.Infrastructure;
+using Serilog;
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddPresentation(builder.Configuration); 
@@ -10,31 +15,12 @@ app.UseGlobalExceptionHandling();
 
 if (app.Environment.IsDevelopment())
 {
-    app.MapOpenApi();
+    app.UseOpenApiUi();
 }
 
-var summaries = new[]
-{
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
+app.UseHttpsRedirection();
+app.UseAuthorization();
 
-app.MapGet("/weatherforecast", () =>
-{
-    var forecast =  Enumerable.Range(1, 5).Select(index =>
-        new WeatherForecast
-        (
-            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-            Random.Shared.Next(-20, 55),
-            summaries[Random.Shared.Next(summaries.Length)]
-        ))
-        .ToArray();
-    return forecast;
-})
-.WithName("GetWeatherForecast");
+app.MapControllers();
 
 app.Run();
-
-record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
-{
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-}
